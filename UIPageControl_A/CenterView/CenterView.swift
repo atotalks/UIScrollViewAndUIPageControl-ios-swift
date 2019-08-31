33cddd16bf2c741828402b6fb6826767df06a43e
JuneK <atotalks@gmail.com>
2019-08-31 11:11:44 +0900

    first commit

---
+++ UIPageControl_A/CenterView/CenterView.swift
@@ -0,0 +1,23 @@
+//
+//  CenterView.swift
+//  UIPageControl_A
+//
+//  Created by JuneK on 31/08/2019.
+//  Copyright © 2019 JuneK. All rights reserved.
+//
+
+import UIKit
+
+class CenterView: UIView {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ UIPageControl_A/ViewController.swift
@@ -0,0 +1,102 @@
+//
+//  ViewController.swift
+//  UIPageControl_A
+//
+//  Created by JuneK on 31/08/2019.
+//  Copyright © 2019 JuneK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIScrollViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        pageControl.numberOfPages = slides.count
+        pageControl.currentPage = 0
+        
+        view.bringSubviewToFront(pageControl)
+    }
+
+    @IBOutlet weak var scrollView: UIScrollView!{
+        didSet{
+            scrollView.delegate = self
+        }
+    }
+    
+    @IBOutlet weak var pageControl: UIPageControl!
+    
+    var slides:[CenterView] = [];
+    
+    func createSlides() -> [CenterView] {
+        
+        let slide1:CenterView = Bundle.main.loadNibNamed("CenterView", owner: self, options: nil)?.first as! CenterView
+        slide1.imageView.image = UIImage(named: "ar")
+        
+        let slide2:CenterView = Bundle.main.loadNibNamed("CenterView", owner: self, options: nil)?.first as! CenterView
+        slide2.imageView.image = UIImage(named: "ml")
+        
+        let slide3:CenterView = Bundle.main.loadNibNamed("CenterView", owner: self, options: nil)?.first as! CenterView
+        slide3.imageView.image = UIImage(named: "siri")
+        
+        return [slide1, slide2, slide3]
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        pageControl.currentPage = Int(pageIndex)
+    }
+    
+    func setupSlideScrollView(slides : [CenterView]) {
+        scrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        scrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        scrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            scrollView.addSubview(slides[i])
+        }
+    }
+    
+    func scrollView(_ scrollView: UIScrollView, didScrollToPercentageOffset percentageHorizontalOffset: CGFloat) {
+        if(pageControl.currentPage == 0) {
+            
+            let pageUnselectedColor: UIColor = fade(fromRed: 255/255, fromGreen: 255/255, fromBlue: 255/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            pageControl.pageIndicatorTintColor = pageUnselectedColor
+            
+            
+            let bgColor: UIColor = fade(fromRed: 103/255, fromGreen: 58/255, fromBlue: 183/255, fromAlpha: 1, toRed: 255/255, toGreen: 255/255, toBlue: 255/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            slides[pageControl.currentPage].backgroundColor = bgColor
+            
+            let pageSelectedColor: UIColor = fade(fromRed: 81/255, fromGreen: 36/255, fromBlue: 152/255, fromAlpha: 1, toRed: 103/255, toGreen: 58/255, toBlue: 183/255, toAlpha: 1, withPercentage: percentageHorizontalOffset * 3)
+            pageControl.currentPageIndicatorTintColor = pageSelectedColor
+        }
+    }
+    
+    func fade(fromRed: CGFloat,
+              fromGreen: CGFloat,
+              fromBlue: CGFloat,
+              fromAlpha: CGFloat,
+              toRed: CGFloat,
+              toGreen: CGFloat,
+              toBlue: CGFloat,
+              toAlpha: CGFloat,
+              withPercentage percentage: CGFloat) -> UIColor {
+        
+        let red: CGFloat = (toRed - fromRed) * percentage + fromRed
+        let green: CGFloat = (toGreen - fromGreen) * percentage + fromGreen
+        let blue: CGFloat = (toBlue - fromBlue) * percentage + fromBlue
+        let alpha: CGFloat = (toAlpha - fromAlpha) * percentage + fromAlpha
+        
+        // return the fade colour
+        return UIColor(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
+    
+}
+
